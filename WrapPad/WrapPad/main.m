ad8adfaf2cb59e4775ddf03bc046aaa0248eec1b
Sam Tarakajian <starakajian@gmail.com>
2012-03-04 21:02:41 -0800

    Added initial xcode project and some test sounds

---
+++ WrapPad/WrapPad/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WrapPad
+//
+//  Created by Sam Tarakajian on 3/4/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
